e26953910fac58de3b0c58e5d25167491c595a8f
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-28 11:58:00 +0200

    cookies

--- js/cookies.js
+++ js/cookies.js
@@ -15,3 +15,48 @@ console.log('sausainiai ===', sausainiai);
 // cookie istrynimas - nustatyti data i praeiti
 const past = new Date('12/05/2000');
 document.cookie = `age=45; expires=${past.toUTCString()}`;
+
+// taikomes
+const els = {
+  form: document.forms[0],
+  hideBtn: document.getElementById('hide'),
+};
+
+// tik atejus i psl ziurim ar yra nustatytas showForm cookis
+// jei yra slepiam forma
+const showFormCookie = getCookie('showForm');
+console.log('showFormCookie ===', showFormCookie);
+if (showFormCookie !== false) {
+  els.form.classList.add('d-none');
+}
+
+els.hideBtn.addEventListener('click', () => {
+  // uzdeti formai display none
+  els.form.classList.add('d-none');
+  // nustatyti cookie 3 dienom
+  setCookie('showForm', 'true', 3);
+});
+
+// console.log('last', getCookie('showForm'));
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+}
+
+function getCookie(cname) {
+  let name = cname + '=';
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return false;
+}
